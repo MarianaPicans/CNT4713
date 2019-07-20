aad115a2f391b5aaef1476ab218277f75f9d29fd
MarianaPicans <35390935+MarianaPicans@users.noreply.github.com>
2019-07-20 01:55:24 -0400

    Delete form.php

--- Assignment_11/form.php
+++
@@ -1,6 +0,0 @@
-<form action="rsa.php" method = "post" />
-<p>Please enter Plain Text if you want to Encrypt or enter Encrypted Text if you want to Decrypt</p>
-<p>Plain Text: <input type="text" name="encrypted"/></p>
-<p>Encrypted Text: <input type="text" name="decrypted"/></p>
-<input type="submit" value="Submit"/>
-</form>
